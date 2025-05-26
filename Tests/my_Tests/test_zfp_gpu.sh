615c6aa2cb76f1bbb6c08860794491ca04393e19
Georgios Skondras <gskondras@ac.upatras.gr>
2025-05-26 23:30:37 +0300

    zfp_gpu builds with existing building system

---
+++ Tests/my_Tests/test_zfp_gpu.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+# test_zfp.sh
+# CubismZ
+#
+# Copyright 2018 ETH Zurich. All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+#
+set -x #echo on
+
+[[ ! -f ../Data/demo.h5 ]] && tar -C ../Data -xJf ../Data/data.tar.xz
+h5file=../Data/demo.h5
+
+if [ -z ${1+x} ]
+then
+	echo "setting err=0.005"
+	err=0.005
+else
+	err=$1
+	if [ "$1" -eq "-1" ]; then
+		echo "setting err=0.005"
+		err=0.005
+	fi
+	shift
+fi
+
+nproc=1
+if [ ! -z ${1+x} ]
+then
+    nproc=$1; shift
+fi
+
+bs=32
+ds=128
+nb=$(echo "$ds/$bs" | bc)
+
+rm -f tmp.cz
+
+# check if reference file exists, create it otherwise
+if [ ! -f ref.cz ]
+then
+    ./genref.sh
+fi
+
+export OMP_NUM_THREADS=$nproc
+# mpirun -n 1 ../../Tools/bin/zfp/hdf2cz -bpdx $nb -bpdy $nb -bpdz $nb -sim io -h5file $h5file -czfile tmp.cz -threshold $err
+mpirun -n 1 ../../Tools/bin/zfp_gpu/hdf2cz -bpdx $nb -bpdy $nb -bpdz $nb -sim io -h5file $h5file -czfile tmp.cz -threshold $err
+
+
+# mpirun -n $nproc ../../Tools/bin/zfp/cz2diff -czfile1 tmp.cz  -czfile2 ref.cz
