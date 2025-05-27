ff520a6de0262801b8a548c27fa48b489ea9479e
Georgios Skondras <gskondras@ac.upatras.gr>
2025-05-27 19:58:48 +0300

    fixed zfp_gpu lib binaries getting staged by git

--- ThirdParty/zfp_gpu/zfp/builds/build_gpu/src/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gskondras/GPUbismZ/ThirdParty/zfp_gpu/zfp/src
-# Build directory: /home/gskondras/GPUbismZ/ThirdParty/zfp_gpu/zfp/builds/build_gpu/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("cuda_zfp")
--- ThirdParty/zfp_gpu/zfp/builds/build_gpu/utils/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gskondras/GPUbismZ/ThirdParty/zfp_gpu/zfp/utils
-# Build directory: /home/gskondras/GPUbismZ/ThirdParty/zfp_gpu/zfp/builds/build_gpu/utils
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
